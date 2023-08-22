05318e9947f7e89ddfdd73aec91c58efa48b6120
Pham Truong Giang <giangfnptit@gmail.com>
2023-08-22 17:21:00 +0700

    Add final assignment etl kafka airflow

---
+++ ibm_data_engineering/etl_kafka_airflow/final_assignment/ETL_toll_data.py
@@ -0,0 +1,69 @@
+from datetime import timedelta
+# The DAG object; we'll need this to instantiate a DAG
+from airflow import DAG
+# Operators; we need this to write tasks!
+from airflow.operators.bash_operator import BashOperator
+# This makes scheduling easy
+from airflow.utils.dates import days_ago
+
+# Defining DAG arguments
+
+default_args = {
+    'owner': 'Ocean Fish',
+    'start_date': days_ago(0),
+    'email': ['oceanfish888@dummy.com'],
+    'email_on_failure': True,
+    'email_on_retry': True,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+}
+
+# Define the DAG
+
+dag = DAG(
+    'ETL_toll_data',
+    default_args=default_args,
+    description='Apache Airflow Final Assignment',
+    schedule_interval=timedelta(days=1),
+)
+
+# Define the tasks
+
+unzip_data = BashOperator(
+    task_id='unzip_data',
+    bash_command='tar -zxf /home/project/airflow/dags/finalassignment/tolldata.tgz',
+    dag=dag,
+)
+
+extract_data_from_csv = BashOperator(
+    task_id='extract_data_from_csv',
+    bash_command='cut -d"," -f1-4 vehicle-data.csv > csv_data.csv',
+    dag=dag,
+)
+
+extract_data_from_tsv = BashOperator(
+    task_id='extract_data_from_tsv',
+    bash_command='cut -f5-7 tollplaza-data.tsv > tsv_data.csv',
+    dag=dag,
+)
+
+extract_data_from_fixed_width = BashOperator(
+    task_id='extract_data_from_fixed_width',
+    bash_command='cat payment-data.txt | tr -s "[:space:]" | cut -d" " -f11,12 > fixed_width_data.csv',
+    dag=dag,
+)
+
+consolidate_data = BashOperator(
+    task_id='consolidate_data',
+    bash_command='paste csv_data.csv tsv_data.csv fixed_width_data.csv > extracted_data.csv',
+    dag=dag,
+)
+
+transform_data = BashOperator(
+    task_id='transform_data',
+    bash_command='tr "[a-z]" "[A-Z]" < extracted-data.csv > transformed-data.csv',
+    dag=dag,
+)
+
+# task pipeline
+unzip_data >> extract_data_from_csv >> extract_data_from_tsv >> extract_data_from_fixed_width >> consolidate_data >> transform_data
