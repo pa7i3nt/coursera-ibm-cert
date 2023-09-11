2b9aa311576a6158e8f3bcf795b522e155cf4070
pa7i3nt <giangfnptit@gmail.com>
2023-09-11 16:58:20 +0700

    Add final assignment nosql databases

--- ibm_data_engineering/data_warehouse_bi_analytics/ocr_multiple_pdf_in_folder.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-for file in *.pdf; do
-	ocrmypdf "${file}" "${file%.*}_ocr.pdf"
-done;
-
--- ocr_multiple_pdf_in_folder.sh
+++ ocr_multiple_pdf_in_folder.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
+
+echo 'Checking if ocrmypdf library exists...'
+
+if ! command -v ocrmypdf &> /dev/null
+then
+  echo "ocrmypdf not found. Installing..."
+  sudo apt install ocrmypdf
+fi
+
+echo 'Done.'
+
+echo 'Ocr-ing...'
+
 for file in *.pdf; do
 	ocrmypdf "${file}" "${file%.*}_ocr.pdf"
 done;
 
+echo 'Have a good day.'
